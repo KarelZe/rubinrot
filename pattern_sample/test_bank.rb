3546589062a8498d6ca290fd023d982914797c2b
Markus <mail@markusbilz.com>
2019-01-16 14:00:59 +0100

    Anpassung Beispiel Proxy

--- pattern_sample/test_bank.rb
+++ pattern_sample/test_bank.rb
@@ -1,14 +1,14 @@
 require './account'
 require './account_protection_proxy'
 
-account = Account.new 100
-account.deposit(50)
-account.withdraw(10)
+real_account = Account.new 100
+real_account.deposit(50)
+real_account.withdraw(10)
 
-proxy = AccountProtectionProxy.new(account, 'mail')
+proxy = AccountProtectionProxy.new(real_account, 'mail')
 puts proxy.to_s
 proxy.deposit(50)
-puts proxy.to_s
+puts proxy
 proxy.withdraw(10)
-puts proxy.to_s
+puts proxy
 
